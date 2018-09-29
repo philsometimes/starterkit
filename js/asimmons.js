e3222793ce9d50250430469aa8a346b377ccfbfe
Phil <phil@jules.lan>
2018-09-29 18:27:23 -0400

    d

--- js/asimmons.js
+++ js/asimmons.js
@@ -1,4 +1,4 @@
 // JS SPECIFIC TO THIS CUSTOM THEME GOES HERE
 var title = document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML;
 console.log(title);
-document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML = "<h1><a href="/alisonsimmons">Alison Simmons</a></h1><p>Samuel H. Wolcott Professor of Philosophy</p><p>Harvard College Professor (2011-2016)</p>"
+document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML = "<h1>DOES THIS WORK?</h1>";
