2ab4504047d55175a946ef5bd0b00a65dbee797f
Phil <phil@jules.lan>
2018-09-29 18:22:55 -0400

    d

--- js/asimmons.js
+++ js/asimmons.js
@@ -1,3 +1,5 @@
 // JS SPECIFIC TO THIS CUSTOM THEME GOES HERE
-var title = document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML
+var title = document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML;
 console.log(title);
+document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML = "<h1><a href="/alisonsimmons">Alison Simmons</a></h1><p>Samuel H. Wolcott Professor of Philosophy</p><p>
+Harvard College Professor (2011-2016)</p>""
