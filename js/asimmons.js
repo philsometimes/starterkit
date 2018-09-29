f15e23026f1b3aef42e67f02d19525e538ba6367
Phil <phil@jules.lan>
2018-09-29 18:19:48 -0400

    new

--- js/asimmons.js
+++ js/asimmons.js
@@ -1,3 +1,3 @@
 // JS SPECIFIC TO THIS CUSTOM THEME GOES HERE
-var title = document.querySelector("#boxes-box-site_info .boxes-box-content h1").innerHTML
+var title = document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML
 console.log(title);
