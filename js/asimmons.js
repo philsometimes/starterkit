a7c191a0a54e31d1d83043f2b3c36e9a7a3469a7
Phil <phil@jules.lan>
2018-10-06 19:35:55 -0400

    s

--- js/asimmons.js
+++ js/asimmons.js
@@ -1,5 +1,5 @@
 // JS SPECIFIC TO THIS CUSTOM THEME GOES HERE
-var title = document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML;
-console.log(title);
-// document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML = '<h1><a href="/alisonsimmons">Alison Simmons</a></h1><p>Samuel H. Wolcott Professor of Philosophy</p><p>Harvard College Professor (2011-2016)</p>';
-document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML = '<div style="display: inline-block; margin-right:2em;"><h1><a href="/alisonsimmons">Alison Simmons</a></h1></div><div style="display: inline-block;"><p>Samuel H. Wolcott Professor of Philosophy</p><p>Harvard College Professor (2011-2016)</p></div>';
+// var title = document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML;
+// console.log(title);
+// // document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML = '<h1><a href="/alisonsimmons">Alison Simmons</a></h1><p>Samuel H. Wolcott Professor of Philosophy</p><p>Harvard College Professor (2011-2016)</p>';
+// document.querySelector("#boxes-box-site_info .boxes-box-content").innerHTML = '<div style="display: inline-block; margin-right:2em;"><h1><a href="/alisonsimmons">Alison Simmons</a></h1></div><div style="display: inline-block;"><p>Samuel H. Wolcott Professor of Philosophy</p><p>Harvard College Professor (2011-2016)</p></div>';
